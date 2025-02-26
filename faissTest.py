a69eaac603994dad1085eded188ff44a6a415ea4
pantoner <erichorozco@yahoo.com>
2025-02-26 09:38:36 -0500

    first update new branch

--- faissTest.py
+++
@@ -1,8 +0,0 @@
-from faiss_helper import search_faiss
-
-query = "What should I do for my speed workout today?"
-results = search_faiss(query, top_k=3)
-
-print("🔍 FAISS Search Results:")
-for result in results:
-    print(result)
