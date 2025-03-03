773c1518fe35d2e441d769c15738d8cabeadb2a9
pantoner <erichorozco@yahoo.com>
2025-03-03 17:30:57 -0500

    added chat request

--- routes/profile_router.py
+++ routes/profile_router.py
@@ -3,10 +3,13 @@ from pydantic import BaseModel
 from typing import List, Optional
 from datetime import date
 from db import get_user_profile, save_user_profile, get_user_by_email
+from main import ChatRequest
 
 # Import authentication functions from auth_router
 from .auth import get_current_user
 
+class ChatRequest(BaseModel):
+    message: str
 
 profile_router = APIRouter()
 
